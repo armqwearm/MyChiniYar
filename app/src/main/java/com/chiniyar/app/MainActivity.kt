47b34d3f9eeef8e82692a81ba6e0e69d57e81819
armqwearm <1390arm@gmail.com>
2026-08-28 19:43:04 +0330

    refactor: consume application-scoped dependency graph

--- app/src/main/java/com/chiniyar/app/MainActivity.kt
+++ app/src/main/java/com/chiniyar/app/MainActivity.kt
@@ -4,20 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import com.chiniyar.app.di.AppContainer
 import com.chiniyar.app.ui.navigation.AppNavHost
 import com.chiniyar.app.ui.theme.MyChiniYarTheme
 
 class MainActivity : ComponentActivity() {
-    private val appContainer by lazy { AppContainer() }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val app = application as ChiniYarApplication
         setContent {
             MyChiniYarTheme {
                 AppNavHost(
                     navController = rememberNavController(),
-                    appContainer = appContainer
+                    appContainer = app.appContainer
                 )
             }
         }
