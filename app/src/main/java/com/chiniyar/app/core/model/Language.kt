85262493953172a14f210900b1bfce6077206bf9
armqwearm <1390arm@gmail.com>
2026-08-28 19:30:04 +0330

    feat: add core language model

---
+++ app/src/main/java/com/chiniyar/app/core/model/Language.kt
@@ -0,0 +1,8 @@
+package com.chiniyar.app.core.model
+
+/** Languages currently supported by ChiniYar's core translation domain. */
+enum class Language(val code: String) {
+    CHINESE("zh"),
+    PERSIAN("fa"),
+    ENGLISH("en")
+}
