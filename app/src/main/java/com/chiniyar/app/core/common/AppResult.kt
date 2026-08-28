2e64033d7006bb21d7a4368d668608e55f719e88
armqwearm <1390arm@gmail.com>
2026-08-28 19:30:21 +0330

    feat: add shared application result type

---
+++ app/src/main/java/com/chiniyar/app/core/common/AppResult.kt
@@ -0,0 +1,7 @@
+package com.chiniyar.app.core.common
+
+sealed interface AppResult<out T> {
+    data class Success<T>(val data: T) : AppResult<T>
+    data class Error(val message: String, val cause: Throwable? = null) : AppResult<Nothing>
+    data object Loading : AppResult<Nothing>
+}
