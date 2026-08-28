f04fbf2ae2a34c0a6529b13d8f4a2a21e70318b9
armqwearm <1390arm@gmail.com>
2026-08-28 19:28:17 +0330

    feat: introduce typed application destinations

---
+++ app/src/main/java/com/chiniyar/app/ui/navigation/AppDestination.kt
@@ -0,0 +1,11 @@
+package com.chiniyar.app.ui.navigation
+
+sealed interface AppDestination {
+    val route: String
+
+    data object Home : AppDestination { override val route = "home" }
+    data object Translator : AppDestination { override val route = "translator" }
+    data object CameraTranslator : AppDestination { override val route = "camera_translator" }
+    data object Learning : AppDestination { override val route = "learning" }
+    data object Cities : AppDestination { override val route = "cities" }
+}
