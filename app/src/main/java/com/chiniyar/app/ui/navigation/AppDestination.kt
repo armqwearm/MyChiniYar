a8aea8ccc793d668f2d5bd7444705f4b00d4686b
armqwearm <1390arm@gmail.com>
2026-08-28 19:44:03 +0330

    feat: add dictionary navigation destination

--- app/src/main/java/com/chiniyar/app/ui/navigation/AppDestination.kt
+++ app/src/main/java/com/chiniyar/app/ui/navigation/AppDestination.kt
@@ -5,6 +5,7 @@ sealed interface AppDestination {
 
     data object Home : AppDestination { override val route = "home" }
     data object Translator : AppDestination { override val route = "translator" }
+    data object Dictionary : AppDestination { override val route = "dictionary" }
     data object CameraTranslator : AppDestination { override val route = "camera_translator" }
     data object Learning : AppDestination { override val route = "learning" }
     data object Cities : AppDestination { override val route = "cities" }
