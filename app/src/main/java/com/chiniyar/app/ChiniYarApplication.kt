0c3bdeb0703bae591e071fe200d4d3dc45ff3af3
armqwearm <1390arm@gmail.com>
2026-08-31 02:17:07 +0330

    fix: initialize offline dictionary at application startup

--- app/src/main/java/com/chiniyar/app/ChiniYarApplication.kt
+++ app/src/main/java/com/chiniyar/app/ChiniYarApplication.kt
@@ -1,8 +1,14 @@
 package com.chiniyar.app
 
 import android.app.Application
+import com.chiniyar.app.data.analysis.OfflineChineseDictionary
 import com.chiniyar.app.di.AppContainer
 
 class ChiniYarApplication : Application() {
     val appContainer: AppContainer by lazy { AppContainer(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        OfflineChineseDictionary.initialize(this)
+    }
 }
