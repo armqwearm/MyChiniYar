a06be66f11b6dfbc7c88cc1484548804947e47d9
armqwearm <1390arm@gmail.com>
2026-09-01 02:12:06 +0330

    fix: opt into experimental material3 api in translator screen

--- app/src/main/java/com/chiniyar/app/ui/screens/translator/TranslatorScreen.kt
+++ app/src/main/java/com/chiniyar/app/ui/screens/translator/TranslatorScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -46,6 +47,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.chiniyar.app.core.model.Language
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TranslatorScreen(
     viewModel: TranslatorViewModel,
