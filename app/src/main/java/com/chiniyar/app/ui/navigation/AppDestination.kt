60e7123563017562c7b0cd915f81415ade3ddb83
armqwearm <1390arm@gmail.com>
2026-09-14 19:44:24 +0330

    feat: add urban routes destination

--- app/src/main/java/com/chiniyar/app/ui/navigation/AppDestination.kt
+++ app/src/main/java/com/chiniyar/app/ui/navigation/AppDestination.kt
@@ -11,4 +11,5 @@ sealed interface AppDestination {
     data object TravelPhrases : AppDestination { override val route = "travel_phrases" }
     data object Learning : AppDestination { override val route = "learning" }
     data object Cities : AppDestination { override val route = "cities" }
+    data object UrbanRoutes : AppDestination { override val route = "urban_routes" }
 }
