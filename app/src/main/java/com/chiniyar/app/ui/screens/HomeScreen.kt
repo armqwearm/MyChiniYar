46dbdafd025b467843476a2cce5898a487a0114f
armqwearm <1390arm@gmail.com>
2026-09-16 09:47:26 +0330

    Fix HomeScreen alpha import

--- app/src/main/java/com/chiniyar/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/chiniyar/app/ui/screens/HomeScreen.kt
@@ -38,7 +38,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.alpha
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
