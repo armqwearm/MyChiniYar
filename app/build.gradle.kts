e5689daeb6e67b5ba39d182dbca7fb5fd7734ee4
armqwearm <1390arm@gmail.com>
2026-09-19 18:31:20 +0330

    release: bump app to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.chiniyar.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
         }
