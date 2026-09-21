473099f2f221cc3a69d4827a768c42d299b1e694
armqwearm <1390arm@gmail.com>
2026-09-21 17:24:49 +0330

    ui: use bright travel background globally

--- app/src/main/java/com/chiniyar/app/MainActivity.kt
+++ app/src/main/java/com/chiniyar/app/MainActivity.kt
@@ -3,8 +3,14 @@ package com.chiniyar.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.chiniyar.app.ui.navigation.AppNavHost
+import com.chiniyar.app.ui.screens.TravelBackground
 import com.chiniyar.app.ui.theme.MyChiniYarTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,10 +19,21 @@ class MainActivity : ComponentActivity() {
         val app = application as ChiniYarApplication
         setContent {
             MyChiniYarTheme {
-                AppNavHost(
-                    navController = rememberNavController(),
-                    appContainer = app.appContainer
-                )
+                Box(modifier = Modifier.fillMaxSize().background(Color(0xFFFFFBF2))) {
+                    TravelBackground(
+                        modifier = Modifier.fillMaxSize(),
+                        alpha = 0.84f
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White.copy(alpha = 0.08f))
+                    )
+                    AppNavHost(
+                        navController = rememberNavController(),
+                        appContainer = app.appContainer
+                    )
+                }
             }
         }
     }
